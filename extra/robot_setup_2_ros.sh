30882b347a9fe27095dd2ca0d10cf22fbfc9c070
Josh <josh@javconcepts.com>
2013-03-29 23:37:50 -0700

    got the xbox controller up and running, removing support for the wiimote for now

--- extra/robot_setup_2_ros.sh
+++ extra/robot_setup_2_ros.sh
@@ -8,4 +8,4 @@
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu precise main" > /etc/apt/sources.list.d/ros-latest.list'
 wget http://packages.ros.org/ros.key -O - | sudo apt-key add -
 sudo apt-get update
-sudo apt-get install -y ros-groovy-desktop-full ros-groovy-openni-camera ros-groovy-openni-launch ros-groovy-robot-model ros-groovy-robot-model-visualization ros-groovy-rosserial
+sudo apt-get install -y ros-groovy-desktop-full ros-groovy-openni-camera ros-groovy-openni-launch ros-groovy-robot-model ros-groovy-robot-model-visualization ros-groovy-rosserial ros-groovy-joystick-drivers
--- src/teleop_xbox.cpp
+++ src/teleop_xbox.cpp
@@ -1,16 +1,14 @@
 /*
- * File: mecanumbot/src/teleop_joy.cpp
+ * File: mecanumbot/src/teleop_xbox.cpp
  * Author: Josh Villbrandt <josh@javconcepts.com>
- * Date: October 2012 - November 2012
- * Description: This application produces a twist message from a Wiimote or Xbox controller joy message.
+ * Date: October 2012 - March 2013
+ * Description: This application produces a twist message from an Xbox controller joy message.
  */
 
 #include <ros/ros.h>
 #include <ros/console.h>
 #include <geometry_msgs/Twist.h>
 #include <sensor_msgs/Joy.h>
-#include <wiimote/LEDControl.h>
-#include <wiimote/TimedSwitch.h>
 
 class MecanumbotTeleop
 {
@@ -22,11 +20,10 @@ class MecanumbotTeleop
 		void joyCallback(const sensor_msgs::Joy::ConstPtr& joy);
 
 		ros::NodeHandle nh;
-		ros::Publisher led_pub;
 		ros::Publisher vel_pub;
 		ros::Subscriber joy_sub;
 		
-		bool enabled, flipped, wii_lights;
+		bool enabled, flipped;
 		int linear_x_axis, linear_y_axis, angular_z_axis;
 		int linear_y_left_button, linear_y_right_button, boost_button, enable_button;
 		double linear_x_scale, linear_y_scale, angular_z_scale, preboost_scale, force_pub_rate;
@@ -34,9 +31,6 @@ class MecanumbotTeleop
 		geometry_msgs::Twist msg;
 		ros::Time last_pub_time;
 		ros::Duration force_pub_period;
-		
-		std::vector<wiimote::TimedSwitch> enabledLedArray;
-		std::vector<wiimote::TimedSwitch> disabledLedArray;
 };
 
 
@@ -45,7 +39,7 @@ MecanumbotTeleop::MecanumbotTeleop():
 	flipped(false)
 {
 	// load parameters
-    ros::NodeHandle nh_priv("~");
+	ros::NodeHandle nh_priv("~");
 	nh_priv.param("linear_x_scale", linear_x_scale, 0.1); // scaling from integer 10 (1g in m/s/s); 0.1 -> 1m/s
 	nh_priv.param("linear_y_scale", linear_y_scale, 0.8);
 	nh_priv.param("angular_z_scale", angular_z_scale, 0.2);
@@ -58,7 +52,6 @@ MecanumbotTeleop::MecanumbotTeleop():
 	nh_priv.param("linear_y_right_button", linear_y_right_button, -1);
 	nh_priv.param("boost_button", boost_button, -1);
 	nh_priv.param("enable_button", enable_button, -1);
-	nh_priv.param("wii_lights", wii_lights, false);
 	if(enable_button < 0) enabled = true;
 	
 	// lets show em what we got
@@ -74,10 +67,8 @@ MecanumbotTeleop::MecanumbotTeleop():
 	ROS_INFO_STREAM("param linear_y_right_button: " << linear_y_right_button);
 	ROS_INFO_STREAM("param boost_button: " << boost_button);
 	ROS_INFO_STREAM("param enable_button: " << enable_button);
-	ROS_INFO_STREAM("param wii_lights: " << wii_lights);
 
 	// connects subs and pubs
-	if(wii_lights) led_pub = nh.advertise<wiimote::LEDControl>("/wiimote/leds", 1);
 	vel_pub = nh.advertise<geometry_msgs::Twist>("cmd_vel", 1);
 	joy_sub = nh.subscribe<sensor_msgs::Joy>("joy", 1, &MecanumbotTeleop::joyCallback, this);
 
@@ -85,28 +76,10 @@ MecanumbotTeleop::MecanumbotTeleop():
 	msg.linear.x = 0; // m/s
 	msg.angular.z = 0; // rad/s
 	msg.linear.y = 0; // m/s
-	
-	// create enabled / disabled LED arrays
-	if(wii_lights) {
-		wiimote::TimedSwitch onState;
-		onState.switch_mode = wiimote::TimedSwitch::ON;
-		wiimote::TimedSwitch offState;
-		offState.switch_mode = wiimote::TimedSwitch::OFF;
-		enabledLedArray = std::vector<wiimote::TimedSwitch>();
-		enabledLedArray.push_back(offState);
-		enabledLedArray.push_back(onState);
-		enabledLedArray.push_back(onState);
-		enabledLedArray.push_back(offState);
-		disabledLedArray = std::vector<wiimote::TimedSwitch>();
-		disabledLedArray.push_back(onState);
-		disabledLedArray.push_back(offState);
-		disabledLedArray.push_back(offState);
-		disabledLedArray.push_back(onState);
-    }
-    
-    // setup 
-    last_pub_time = ros::Time::now();
-    force_pub_period = ros::Duration(1.0 / force_pub_rate);
+
+	// setup 
+	last_pub_time = ros::Time::now();
+	force_pub_period = ros::Duration(1.0 / force_pub_rate);
 }
 
 void MecanumbotTeleop::spin()
@@ -131,7 +104,7 @@ void MecanumbotTeleop::joyCallback(const sensor_msgs::Joy::ConstPtr& joy)
 	
 	// TODO: cycle hazard lights on/off with button 2
 	
-	// enable / disable cmd_vel commands with the A button
+	// enable / disable cmd_vel commands
 	if(enable_button >= 0) {
 		if(joy->buttons[enable_button] == 0) flipped = false;
 		if(joy->buttons[enable_button] == 1 && !flipped) {
@@ -156,17 +129,9 @@ void MecanumbotTeleop::joyCallback(const sensor_msgs::Joy::ConstPtr& joy)
 			msg.linear.y = msg.linear.y * preboost_scale;
 			msg.angular.z = msg.angular.z * preboost_scale;
 		}
-    }
-    vel_pub.publish(msg);
+	}
+	vel_pub.publish(msg);
 	last_pub_time = ros::Time::now();
-    
-    // set lights based on wiimote enable / disable
-    if(wii_lights) {
-		wiimote::LEDControl ledMsg;
-		if(enabled) ledMsg.timed_switch_array = enabledLedArray;
-		else ledMsg.timed_switch_array = disabledLedArray;
-		led_pub.publish(ledMsg);
-    }
 }
 
 
