cd56a4ca6db85b3e8e84e47254fd70667753e9b0
Josh <josh@javconcepts.com>
2013-04-06 15:59:14 -0700

    setting up for new arduino board

--- extra/robot_setup_2_ros.sh
+++ extra/robot_setup_2_ros.sh
@@ -5,7 +5,13 @@
 #
 ####################################
 
+# Install ROS Groovy and supporting packages for the mecanumbot
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu precise main" > /etc/apt/sources.list.d/ros-latest.list'
 wget http://packages.ros.org/ros.key -O - | sudo apt-key add -
 sudo apt-get update
-sudo apt-get install -y ros-groovy-desktop-full ros-groovy-openni-camera ros-groovy-openni-launch ros-groovy-robot-model ros-groovy-robot-model-visualization ros-groovy-rosserial ros-groovy-joystick-drivers
+sudo apt-get install -y ros-groovy-desktop-full ros-groovy-openni-camera ros-groovy-openni-launch ros-groovy-robot-model ros-groovy-robot-model-visualization ros-groovy-rosserial ros-groovy-joystick-drivers ros-groovy-bullet
+
+# Install PCL
+sudo add-apt-repository ppa:v-launchpad-jochen-sprickerhof-de/pcl
+sudo apt-get update
+sudo apt-get install -y --force-yes libpcl-all #force-yes because there is no key
